d7624d9609b84a8d1d380854c76922ace3162c3a
renantatsuo <tatsuorenan@gmail.com>
2021-03-09 19:06:49 -0300

    First custom button  * Added SVG support  * Added Emotion to handle styled components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import styled from "@emotion/native";
+import GoogleIcon from "./assets/google-icon.svg";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LoginContainer>
+      <LoginButton onPress={() => ""}>
+        <GoogleIcon height={30} width={40} />
+        <ButtonText style={{ marginLeft: 10 }}>Sign in with Google</ButtonText>
+      </LoginButton>
+    </LoginContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const LoginContainer = styled.View({
+  flex: 1,
+  backgroundColor: "#fff",
+  justifyContent: "center",
+  paddingHorizontal: 30,
+});
+
+const LoginButton = styled.TouchableOpacity({
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  padding: 15,
+  shadowColor: "#000",
+  shadowOffset: {
+    width: 0.5,
+    height: 2,
   },
+  shadowOpacity: 0.22,
+  shadowRadius: 2.22,
+  elevation: 3,
+  backgroundColor: "#fff",
+});
+
+const ButtonText = styled.Text({
+  fontWeight: "bold",
+  color: "#666",
 });
