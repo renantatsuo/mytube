d95d969b42c0d221d8850627868588fa5b407717
renantatsuo <tatsuorenan@gmail.com>
2021-03-10 20:12:08 -0300

    Setup module and env configs

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "inline-dotenv",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "#assets": "./assets",
+            "#hooks": "./hooks",
+            "#lib": "./lib",
+            "#context": "./context",
+            "#screens": "./screens",
+            "#components": "./components",
+          },
+        },
+      ],
+    ],
   };
 };
